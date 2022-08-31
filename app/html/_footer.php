aad0dc23b251d6131222f3489539b99140d69d35
cworrell <cgulliver@users.noreply.github.com>
2022-08-31 14:36:04 -0400

    Delete app directory

--- app/html/_footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// get template and replace placeholders
-$template .= file_get_contents('includes/footer.html');
-
-// variables
-$output['copyYear'] = date('Y');
--- app/html/_header.php
+++
@@ -1,3 +0,0 @@
-<?php
-// get template and replace placeholders
-$template .= file_get_contents('includes/header.html');
